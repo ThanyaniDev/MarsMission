40ee028d05a6079aa9edc13e33d20166a72336af
ThanyaniTshikonwani <thanyani.tshikonwani@standardbank.co.za>
2020-11-02 13:58:23 +0200

    replaceMe file

---
+++ MarsMission/Models/ReplaceMe.swift
@@ -0,0 +1,8 @@
+//
+//  ReplaceMe.swift
+//  MarsMission
+//
+//  Created by Thanyani on 2020/11/02.
+//
+
+import Foundation
---
+++ MarsMission/ViewModels/ReplaceMeViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ReplaceMeViewModel.swift
+//  MarsMission
+//
+//  Created by Thanyani on 2020/11/02.
+//
+
+import Foundation
