2ea0454b54af54b0be8fbcda94532295d13251ac
Thanyani <roony07tshikonwani@gmail.com>
2020-12-03 11:03:35 +0200

    Added mars api mapping model and add mars endpoint as an string extension

--- MarsMission/Models/ForecastModel.swift
+++ MarsMission/Models/ForecastModel.swift
@@ -21,3 +21,26 @@ struct ForecastData: Codable {
 	var safe: Bool?
 }
 
+struct SolForecast: Codable {
+	var atmosphericTemperature: MarsForecastData
+	var firstUTC: String
+	
+	init(solDict: Dictionary<String, Any>) {
+		self.atmosphericTemperature = MarsForecastData(solForecastDict: solDict["AT"] as! Dictionary<String, Any>)
+		self.firstUTC = solDict["First_UTC"] as! String
+	}
+}
+
+struct MarsForecastData: Codable {
+	var averageValue: Double
+	var recordedSamplesOverSol: Int
+	var minValue: Double
+	var maxValue: Double
+	
+	init(solForecastDict: Dictionary<String, Any>) {
+		self.averageValue = solForecastDict["av"] as! Double
+		self.recordedSamplesOverSol = solForecastDict["ct"] as! Int
+		self.minValue = solForecastDict["mn"] as! Double
+		self.maxValue = solForecastDict["mx"] as! Double
+	}
+}
--- MarsMission/Utilities/String+extension.swift
+++ MarsMission/Utilities/String+extension.swift
@@ -10,6 +10,7 @@ import UIKit
 
 extension String {
 	static let forecastUrl = "https://run.mocky.io/v3/04dc1be1-8609-48c9-b4a0-27a363aa22a9"
+	static let forecastMarsMissionUrl = "https://api.nasa.gov/insight_weather/?api_key=pn9u3Bxs3CjiCtIOlgcsZ0wt8bebWbEtHb5jhEwd&feedtype=json&ver=1.0"
 	static let forecastViewControllerNibName = "ForecastViewController"
 	static let forecastDetailViewControllerNibName = "ForecastDetailViewController"
 	static let forecastNavTitle = "Mars Mission"
